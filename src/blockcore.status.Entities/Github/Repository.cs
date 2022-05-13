528a8e53794a07089bf054fa098b8124d9d6d391
Milad <miladsoft@yahoo.com>
2022-05-13 23:55:21 +0430

    🧑‍💻 add github repository entity

--- src/blockcore.status.Entities/Github/Repository.cs
+++ src/blockcore.status.Entities/Github/Repository.cs
@@ -3,10 +3,12 @@ using Microsoft.AspNetCore.Identity;
 
 namespace blockcore.status.Entities.Admin;
 
-public class Organization : IAuditableEntity
+public class Repository : IAuditableEntity
 {
     public int Id { get; set; }
-    public string OrganizationName { get; set; }
+    public string RepositoryName { get; set; }
     public DateTime? CreatedDateTime { get; set; }
-    public virtual ICollection<Repository> Repository { get; set; }
+    public int OrganizationId { get; set; }
+    public virtual Organization Organization { get; set; }
+
 }
