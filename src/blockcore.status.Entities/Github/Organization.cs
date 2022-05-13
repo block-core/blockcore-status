fa4ffc2d6df331216a405cac7e40125aece0247d
Milad <miladsoft@yahoo.com>
2022-05-13 23:48:52 +0430

    🧑‍💻 add github organization entity

---
+++ src/blockcore.status.Entities/Github/Organization.cs
@@ -0,0 +1,12 @@
+﻿using blockcore.status.Entities.AuditableEntity;
+using Microsoft.AspNetCore.Identity;
+
+namespace blockcore.status.Entities.Admin;
+
+public class Organization : IAuditableEntity
+{
+    public int Id { get; set; }
+    public string OrganizationName { get; set; }
+    public DateTime? CreatedDateTime { get; set; }
+
+}
